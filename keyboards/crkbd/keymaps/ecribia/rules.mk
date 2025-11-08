d903aa4e058a1498da522d2e06c28c1cf5a188e4
Mint <ozz.gaming@proton.me>
2025-11-08 06:53:13 +0100

    add repeat key

--- keyboards/crkbd/keymaps/ecribia/rules.mk
+++ keyboards/crkbd/keymaps/ecribia/rules.mk
@@ -3,6 +3,7 @@ VIA_ENABLE          = yes
 LTO_ENABLE          = yes
 
 MOUSEKEY_ENABLE     = yes
+REPEAT_KEY_ENABLE	= yes
 OLED_ENABLE         = yes
 OLED_DRIVER         = ssd1306
 EXTRAKEY_ENABLE     = no
